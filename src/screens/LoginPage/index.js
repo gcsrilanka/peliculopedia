04cd1f5ea6b08a59b84234729e2d990f1eb1de06
imakish <makishtp@gmail.com>
2021-10-17 16:20:45 +0530

    Added google signin

--- src/screens/LoginPage/index.js
+++ src/screens/LoginPage/index.js
@@ -1,10 +1,38 @@
 import React from "react";
-import './index.css';
+import "./index.css";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// import firebaseConfig from ""                                                    to be imported from where it is going to be defined
 
-function LoginPage() {
+const LoginPage = () => {
+  const signInWithGoogle = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+  const provider = new GoogleAuthProvider();
+
   return (
-    <h1>This is your LoginPage</h1>
+    <div className="login">
+      <button onClick={signInWithGoogle}>Google</button>
+    </div>
   );
-}
+};
 
 export default LoginPage;
