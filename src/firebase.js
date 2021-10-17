4ed322a11e52420320a4fb9f30a78a354d03301a
imakish <makishtp@gmail.com>
2021-10-17 16:56:25 +0530

    changes to previous commit

--- src/firebase.js
+++ src/firebase.js
@@ -15,4 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export default app
+export default app;
--- src/screens/LoginPage/index.js
+++ src/screens/LoginPage/index.js
@@ -1,33 +1,21 @@
 import React from "react";
 import "./index.css";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-// import firebaseConfig from ""                                                    to be imported from where it is going to be defined
 
 const LoginPage = () => {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
   const signInWithGoogle = () => {
-    const auth = getAuth();
     signInWithPopup(auth, provider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
-        // The signed-in user info.
         const user = result.user;
-        // ...
       })
       .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
+        console.log(error);
       });
   };
-  const provider = new GoogleAuthProvider();
-
   return (
     <div className="login">
       <button onClick={signInWithGoogle}>Google</button>
