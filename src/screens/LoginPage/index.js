4c7e9765d39c15277b8552c7f1fd6de123150221
imakish <makishtp@gmail.com>
2021-10-19 13:04:46 +0530

    changed ui as requested

--- src/screens/LoginPage/index.js
+++ src/screens/LoginPage/index.js
@@ -1,33 +1,34 @@
 import React from "react";
 import "./index.css";
 import LoginImg from "../../Images/LoginImg.svg";
-import GoogleLogo from "../../Images/googleIcon.png";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const LoginPage = () => {
-  const auth = getAuth();
-  const provider = new GoogleAuthProvider();
-  const signInWithGoogle = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        const user = result.user;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  // const auth = getAuth();
+  // const provider = new GoogleAuthProvider();
+  // const signInWithGoogle = () => {
+  //   signInWithPopup(auth, provider)
+  //     .then((result) => {
+  //       const credential = GoogleAuthProvider.credentialFromResult(result);
+  //       const token = credential.accessToken;
+  //       const user = result.user;
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
   return (
     <div className="login">
+      <h2 className="header">Sign In</h2>
       <img src={LoginImg} className="banner" alt="login-image" />
-      <div className="card">
-        <h2 className="header">Sign In</h2>
-        <div className="button-wrapper">
-          <button onClick={signInWithGoogle}>
-            <img src={GoogleLogo} className="google-logo" />
-          </button>
-        </div>
+      <div className="button-wrapper">
+        <button
+          type="button"
+          class="login-google"
+          // onClick={signInWithGoogle}
+        >
+          Sign in with Google
+        </button>
       </div>
     </div>
   );
